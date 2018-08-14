54b61a54ea9539720e3a630ef3e78ae1c2539712
RaymundoCC98 <42361736+RaymundoCC98@users.noreply.github.com>
2018-08-13 20:38:02 -0500

    Add files via upload

---
+++ Hola.c
@@ -0,0 +1,16 @@
+/**main.c - Este es un archivo hola mundo
+*incluido en este curso -- solo para evitar
+*la maldici\'on del programador--. En este curso,
++usted aprender \a usar correctamete muchos de .....*/
+#include <stdio.h>  /*printf(char*...) este es un ejemplo de comentario de bloque*/
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+putchar('A');
+putchar('\n');
+putchar('\n');
+printf("Hola mundo C!!\n");
+  system("PAUSE");	
+  return 0;
+}
